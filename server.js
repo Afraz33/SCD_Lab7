3a6688c0f961dda74a856433e10d6292e4be768d
Afraz33 <afraz3301@gmail.com>
2022-10-15 16:57:09 +0500

    First Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+app.get('/',function(req, res){
+res.send('I am Afraz')
+})
+
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+app.listen(3000, () => {
+  connect();
+  console.log("Server is running on port 3000")
+})
